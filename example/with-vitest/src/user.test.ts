a92ef78ad403842c36c7817ee4a8926fc91992b3
Kazuhito Hokamura <k.hokamura@gmail.com>
2024-03-24 13:19:13 +0900

    Add vitest example

---
+++ example/with-vitest/src/user.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { prisma } from "./client";
+import { createUser } from "./user";
+
+describe("user", () => {
+  it("should create a new user", async () => {
+    // this record will delete by prisma-cleaner in afterEach defined by setup.ts
+    const created = await createUser("xxx");
+    expect(created.email).toEqual("xxx");
+    expect(await prisma.user.count()).toEqual(1);
+  });
+
+  it("should be cleanup user table by cleaner", async () => {
+    const count = await prisma.user.count();
+    expect(count).toEqual(0);
+  });
+});
